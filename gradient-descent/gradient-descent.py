1045f4e405b2a3fafbd3b166a3652afdebd0efc9
devmood <32578489+devmood@users.noreply.github.com>
2018-04-01 22:02:21 +0200

    Few minor changes.

--- gradient-descent/gradient-descent.py
+++ gradient-descent/gradient-descent.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def step_gradient(a_current, b_current, etha, data):
+def step_gradient(a_current, b_current, eta, data):
     a_gradient = 0
     b_gradient = 0
     n = float(len(data))
@@ -11,15 +11,15 @@ def step_gradient(a_current, b_current, etha, data):
         y = sample[1]
         a_gradient += -(2/n) * (y - ((b_current * x) + a_current))
         b_gradient += -(2/n) * x * (y - ((b_current * x) + a_current))
-    new_a = a_current - (etha * a_gradient)
-    new_b = b_current - (etha * b_gradient)
+    new_a = a_current - (eta * a_gradient)
+    new_b = b_current - (eta * b_gradient)
     return new_a, new_b
 
 
-def change_coefficients(starting_a, starting_b, iterations):
+def change_coefficients(starting_a, starting_b, epochs):
     a = starting_a
     b = starting_b
-    for _ in range(iterations):
+    for _ in range(epochs):
         a, b = step_gradient(a, b, 0.0001, np.array(data))
     return a, b
 
@@ -37,10 +37,10 @@ if __name__ == '__main__':
     data = [[1, 2], [3, 4], [6, 7]]
     initial_a = 0
     initial_b = 0
-    iterations = 1000
+    epochs = 1000
     print('Before running the gradient descent algorithm...\na: ',\
           initial_a, ', b: ', initial_b, ', error: ',\
           measure_error(initial_a, initial_b, data))
-    a, b = change_coefficients(initial_a, initial_b, iterations)
+    a, b = change_coefficients(initial_a, initial_b, epochs)
     print('After running the gradient descent algorithm...\na: ',\
           a, ', b: ', b, ', error: ', measure_error(a, b, data))
